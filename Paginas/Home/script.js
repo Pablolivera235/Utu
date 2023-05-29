6160d81521a9a7cb8f67132d3dcad4dc6057a9b3
Pablolivera235 <119084792+Pablolivera235@users.noreply.github.com>
2023-05-29 17:56:00 -0300

    Update script.js

--- Paginas/Home/script.js
+++ Paginas/Home/script.js
@@ -14,17 +14,38 @@ $(window).scroll(function () {
    //pixel 590 de scroll para poner la foto
 
    var container = document.getElementById('fundacion');
-   container.innerHTML = '<img id="fund" src="./Fotos/fundacion(editada).jpeg">'
-   var fund1 = document.getElementById("fund");
-   if (pixel < 125) {
-      container.innerHTML = '<style> #fund{opacity:0;} </style>'
-   }
-   else if (pixel > 787){
-      container.innerHTML = '<style> #fund{opacity:0;} </style>'
-   }
+  //  container.innerHTML = ''
+   // var fund1 = document.getElementById("fund");
+   
 
+   /* if (pixel < 125 || pixel > 787) {
+      container.innerHTML = '<style> #fundacion{opacity:0;} </style>'     
+   }
+   else{
+      container.innerHTML = '<style> #fundacion{opacity:1; border:solid;}</style>'  
+   }
+   */
+   
+   var previousScrollPosition = window.pageYOffset;
 
-   //  });
+    function handleScroll() {
+      var element = document.querySelector('.untt');
+      var desiredScrollPosition = 120;
+      var currentScrollPosition = window.pageYOffset;
+
+      if (currentScrollPosition >= desiredScrollPosition && currentScrollPosition > previousScrollPosition) {
+        element.classList.add('animate');
+      } else {
+        element.classList.remove('animate');
+      }
+
+      previousScrollPosition = currentScrollPosition;
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    
 });
+
+
 
 
