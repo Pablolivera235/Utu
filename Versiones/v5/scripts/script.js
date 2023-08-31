412009257dc9249fcfbf98005ebbe0c96353298a
Pablolivera235 <pablo.olivera235@gmail.com>
2023-08-31 17:40:19 -0300

    cambios varios, en html y css de historia, y otros

--- Versiones/v5/scripts/script.js
+++ Versiones/v5/scripts/script.js
@@ -3,12 +3,12 @@ const bkgsec1 = document.querySelectorAll('.cont_img_sec1');
 const divimg1 = document.getElementById("div-img-1");
 
 images.forEach((image) => {
-    image.addEventListener('mouseenter', () => {
-        images.forEach((img) => {
-            img.classList.remove('hovered');
-        });
-        image.classList.add('hovered');
+  image.addEventListener('mouseenter', () => {
+    images.forEach((img) => {
+      img.classList.remove('hovered');
     });
+    image.classList.add('hovered');
+  });
 });
 
 
@@ -16,34 +16,33 @@ images.forEach((image) => {
 // pantalla de carga inicio
 
 function showWaitScreen() {
-    document.querySelector('.wait-screen').style.display = 'flex';
-    document.querySelector('.logo_pantalla_de_carga').style.animation = '3s cliper';
-    document.querySelector('.wait-screen-message').style.animation = '3s moveup';
-  }
+  document.querySelector('.wait-screen').style.display = 'flex';
+  document.querySelector('.logo_pantalla_de_carga').style.animation = '3s cliper';
+  document.querySelector('.wait-screen-message').style.animation = '3s moveup';
+}
 
-  function hideWaitScreen() {
-    document.querySelector('.wait-screen').style.display = 'none';
-  }
+function hideWaitScreen() {
+  document.querySelector('.wait-screen').style.display = 'none';
+}
 
 
-  if (window.performance.navigation.type == window.performance.navigation.TYPE_RELOAD)
-  {
+if (window.performance.navigation.type == window.performance.navigation.TYPE_RELOAD) {
 
-  } else {
-    showWaitScreen();
+} else {
+  showWaitScreen();
 
-  }
+}
 
-  setTimeout(function() {
-    hideWaitScreen();
-  }, 3000); 
+setTimeout(function () {
+  hideWaitScreen();
+}, 3000);
 
 // pantalla de carga fin
 
 
 // al hacer clic en el logo, recarga la pagina sin la pantalla de carga
 
-document.getElementById("reiniciar").addEventListener("click", function(){
+document.getElementById("reiniciar").addEventListener("click", function () {
   var cargando = document.querySelector('.wait-screen');
   cargando.style.display = "none";
   location.reload();
@@ -54,3 +53,6 @@ document.getElementById("reiniciar").addEventListener("click", function(){
 function redireccionarPagina(link) {
   window.location = link;
 }
+
+
+
